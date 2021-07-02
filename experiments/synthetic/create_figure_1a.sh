8df5d419b56fd8baff26515c696b8c23256c7e34
cora1021 <gaura1021@gmail.com>
2021-07-01 22:41:18 -0700

    update

--- experiments/synthetic/create_figure_1a.sh
+++ experiments/synthetic/create_figure_1a.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for n in 2 4 8 16 32 64 128; do
+for n in 16 32 64 128 256 512 1024 2048 4096 8192; do
     python3 experiments/synthetic/run_experiment.py --n=$n --experiment=loss_vs_n
 done
--- experiments/synthetic/create_figure_1b.sh
+++ experiments/synthetic/create_figure_1b.sh
@@ -1,3 +1,3 @@
-for d in 2 4 8 16 32 64 128; do
+for d in 2 4 8 16 32 64 128 256 512 1024; do
     python3 experiments/synthetic/run_experiment.py --d=$d --experiment=loss_vs_d 
 done
